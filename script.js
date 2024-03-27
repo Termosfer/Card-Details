d08689fca38fdb289a762cdb7b7785ac86f318f7
Abbas <mammadovabbas.0@gmail.com>
2024-03-27 16:15:08 +0400

    abbas

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let cardholderinput = document.querySelector(".cardholder");
 let cardname = document.querySelector(".name");
 let cardnumberinput = document.querySelector(".cardnumber");
 let cardnumber = document.querySelector("h1");
-
+let cardDayInput = document.querySelector(".input1");
+let cardDay = document.querySelector("#a");
+let cardYearInput = document.querySelector(".input2");
+let cardYear = document.querySelector("#b");
 // Card Number input alanının keyup event'ini dinleyen fonksiyon
 cardnumberinput.addEventListener("input", () => {
   let formatted = "";
   let cardNumberValue = cardnumberinput.value.replace(/\s/g, ""); // Mevcut boşlukları temizle
+  debugger
   for (let i = 0; i < cardNumberValue.length; i++) {
     if (i > 0 && i % 4 === 0) {
       formatted += " ";
@@ -15,7 +19,6 @@ cardnumberinput.addEventListener("input", () => {
   }
 
   cardnumberinput.value = formatted;
-
   // Card Number'ın innerText'ini değiştir
 });
 
@@ -30,4 +33,11 @@ cardnumberinput.addEventListener("input", () => {
 
 cardholderinput.addEventListener("keyup", () => {
   cardname.innerHTML = cardholderinput.value;
+});
+
+cardDayInput.addEventListener("input", () => {
+  cardDay.innerText = cardDayInput.value;
+});
+cardYearInput.addEventListener("input", () => {
+  cardYear.innerText = cardYearInput.value;
 });
